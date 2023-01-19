31ee31150e00b129e99ca81cf98242c709315a56
Nolann Parcheminer <parcheminer.nolann@gmail.com>
2023-01-19 11:41:07 +0100

    on a un problème

--- src/main/core/Cell.java
+++ src/main/core/Cell.java
@@ -1,13 +1,19 @@
 package main.core;
 
+import org.javatuples.*;
+import java.util.ArrayList;
 
 public class Cell {
-    
+    protected Pair<Integer,Integer> coordinates;
     protected boolean alive;
-    
+    protected ArrayList<Cell> neighbors;
+    protected int maxNeighbors;
 
-    public Cell(boolean alive) {
+    public Cell(Pair<Integer,Integer> coordinates, boolean alive, int maxNeighbors) {
+        this.coordinates = coordinates;
         this.alive = alive;
+        this.neighbors = new ArrayList<Cell>();
+        this.maxNeighbors = maxNeighbors;
     }
 
     public void setState(boolean state) {
@@ -17,6 +23,24 @@ public class Cell {
         return this.alive;
     }
 
-    
-    
+    public Integer getCoordinateX() {
+        return this.coordinates.getValue0();
+    }
+
+    public int getCoordinateY() {
+        return this.coordinates.getValue1();
+    }
+
+    public ArrayList<Cell> getNeighbors(int radius) {
+        return this.neighbors;
+    }
+
+    public int getMaxNeighbors() {
+        return this.maxNeighbors;
+    }
+
+    public void setMaxNeighbors(int maxNeighbors) {
+        this.maxNeighbors = maxNeighbors;
+    }
+
 }
