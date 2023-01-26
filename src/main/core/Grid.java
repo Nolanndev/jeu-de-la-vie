c5555b1ce1f4121a5cb7f6a8fc18f434012cf88c
Bastien Marcheron <marcher211@c304l-403c13.campus.unicaen.fr>
2023-01-26 12:06:20 +0100

    Grid.java qui marche

--- src/main/core/Grid.java
+++ src/main/core/Grid.java
@@ -1,18 +1,20 @@
 package main.core;
 
+import main.utils.Tuple;
+
 public class Grid{
     private int rows;
     private int columns;
-    private int grid[][];
+    private Cell grid[][];
 
-    public Grid(int rows, int columns, int grid[][]){
+    public Grid(int rows, int columns){
         this.rows = rows;
         this.columns = columns;
-        this.grid = new int[rows][columns];
-
-        for (int i = 0; i <= rows; i++){
-            for (int j = 0; j <= columns; j++){
-                grid[i][j] = 0; // on initialise une grille vide de 0. On ajoutera des cellules dedans plutard
+        this.grid = new Cell[rows][columns];
+        
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                grid[i][j] = new Cell(new Tuple(i,j), 0, 0); 
             }
         }
     }
@@ -26,8 +28,8 @@ public class Grid{
     }
 
     public void showGrid(){
-        for (int i = 0; i <= getRows(); i++){
-            for (int j = 0; j <= getColumns(); j++){
+        for (int i = 0; i < getRows(); i++){
+            for (int j = 0; j < getColumns(); j++){
                 System.out.println(grid[i][j]);
             }
         }
