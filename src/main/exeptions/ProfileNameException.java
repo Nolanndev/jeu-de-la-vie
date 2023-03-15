6617fac4dbd76ef9d496a2f6179f3f28ac7ad75f
nolann parcheminer <parchem211@unicaen.fr>
2023-03-15 10:50:37 +0100

    gestion des exceptions dans la partie ProfileManager

---
+++ src/main/exeptions/ProfileNameException.java
@@ -0,0 +1,11 @@
+package main.exeptions;
+
+public class ProfileNameException extends Exception {
+    public ProfileNameException() {
+        super();
+    }
+
+    public ProfileNameException(String s) {
+        super(s);
+    }
+}
--- src/tests/Test.java
+++ src/tests/Test.java
@@ -1,32 +1,36 @@
-package tests;
+// package tests;
 
-import main.core.*;
-
-import tests.utils.TestQuadtree;
-
-import tests.core.TestGrid;
-import tests.core.TestNormalCell;
-import tests.core.TestHashLife;
-
-/*
-Ce fichier sert a tester toutes les fonctions du projet, il sera donc
-MAJ regulierement 
-
-ant compil
-java -cp bin/ tests.Test
-*/
+// import main.core.*;
+// import main.utils.*;
 
 
-public class Test{
+// import tests.utils.*;
+// import tests.core.*;
 
-    public static void main(String[] args){
+// /*
+// Ce fichier sert a tester toutes les fonctions du projet, il sera donc
+// MAJ regulierement 
 
-        TestNormalCell.startTest();
-        TestGrid.startTest();
-        TestQuadtree.startTest(); // TestQuadtree à modifier
+// ant compil
+// java -cp bin/ tests.Test
+// */
 
-        // --- test ok au dessus, en dessous plus trop =')
-        TestHashLife.startTest();    
-    }
+
+// public class Test{
+
+//     public static void main(String[] args){
+
+//         // TestNormalCell.startTest();
+//         // TestGrid.startTest();
+//         // TestQuadtree.startTest(); // TestQuadtree à modifier
+
+//         // // --- test ok au dessus, en dessous plus trop =')
+//         // TestHashLife.startTest();    
+        
+
+//         // TestFileManager.startTest();
+
+
+//     }
     
-}
+// }
