15865a809012834b5049c42538dc6d5443c7388d
unknown <bastien.marcheron@etu.unicaen.fr>
2023-04-11 13:26:56 +0200

    ajout des Tests qui on été supprimé =(

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -1,92 +1,90 @@
-// package tests;
+package tests;
 
-// import java.io.IOException;
-// import main.exceptions.*;
+import java.io.IOException;
+import main.exceptions.*;
 
 
-// import tests.utils.TestProfileManager;
-// import tests.utils.TestQuadtree;
+import tests.utils.TestProfileManager;
+import tests.utils.TestQuadtree;
 
-// import tests.core.TestGrid;
-// import tests.core.TestHashLife;
-// import tests.core.TestCell;
+import tests.core.TestGrid;
+import tests.core.TestHashLife;
+import tests.core.TestCell;
 
-// /*
-// java -ea -cp bin/ tests.Test */
 
-// public class Test{
 
-//     public static void main(String[] args) throws IOException{
+public class Test {
 
-//         boolean ok = true;
+    public static void main(String[] args) throws IOException{
+
+        boolean ok = true;
         
-//         System.out.println("------ProfileManager-------");
-
-
-//         TestProfileManager profileManagerTester = new TestProfileManager();
-//         ok = ok && profileManagerTester.testIsUUID();
-//         ok = ok && profileManagerTester.testLoad();
-
-// /* 
-//         ok = ok && profileManagerTester.testValidProfileName();
-//         ok = ok && profileManagerTester.testSave();
-// */
-// System.out.println("------Quadtree-------");
+        System.out.println("-------------");
+/*
+        // All test OK
+        TestProfileManager profileManagerTester = new TestProfileManager();
+        ok = ok && profileManagerTester.testLoad();
+        ok = ok && profileManagerTester.testValidProfileName();
+        ok = ok && profileManagerTester.testSave();
+*/
+        System.out.println("-------------");
      
-//         // All test OK
-//         TestQuadtree quadtree = new TestQuadtree();
+        // All test OK
+        TestQuadtree quadtree = new TestQuadtree();
 
-//         ok = ok && quadtree.testGetters();
-//         ok = ok && quadtree.testHashCode();
-//         ok = ok && quadtree.testEquals();
-//         ok = ok && quadtree.testToString();
+        ok = ok && quadtree.testGetters();
+        ok = ok && quadtree.testHashCode();
+        ok = ok && quadtree.testEquals();
+        ok = ok && quadtree.testToString();
 
-//         System.out.println("------Grid-------");
+        System.out.println("-------------");
+
        
-//         //All test OK
-//         TestGrid grid = new TestGrid();
-//         ok = ok && grid.testGetters();
-//         ok = ok && grid.testGetSize();
-//         ok = ok && grid.testGetWidth();
-//         ok = ok && grid.testGetHeight();
-//         ok = ok && grid.testGetRows();
-//         ok = ok && grid.testGetColumns();
-//         ok = ok && grid.testCopyBoard();
-//         ok = ok && grid.testCountNeighbors();
-//         ok = ok && grid.testGetCell();
+        //All test OK
+        TestGrid grid = new TestGrid();
+        ok = ok && grid.testGetters();
+        ok = ok && grid.testGetSize();
+        ok = ok && grid.testGetWidth();
+        ok = ok && grid.testGetHeight();
+        ok = ok && grid.testGetRows();
+        ok = ok && grid.testGetColumns();
+        ok = ok && grid.testCopyBoard();
+        ok = ok && grid.testCountNeighbors();
+        ok = ok && grid.testGetCell();
 
-//         System.out.println("------Cell-------");
+        System.out.println("-------------");
 
-//         //All test OK
-//         TestCell cell = new TestCell();
-//         ok = ok && cell.testIsAlive();
-//         ok = ok && cell.testGetRadius();
-//         ok = ok && cell.testGetBornMaxNeighbors();
-//         ok = ok && cell.testGetBornMinNeighbors();
-//         ok = ok && cell.testGetDieMinNeighbors();
-//         ok = ok && cell.testGetDieMaxNeighbors();
+        //All test OK
+        TestCell cell = new TestCell();
+        ok = ok && cell.testIsAlive();
+        ok = ok && cell.testGetRadius();
+        ok = ok && cell.testGetBornMaxNeighbors();
+        ok = ok && cell.testGetBornMinNeighbors();
+        ok = ok && cell.testGetDieMinNeighbors();
+        ok = ok && cell.testGetDieMaxNeighbors();
 
-//         System.out.println("------HashLife-------");
+        System.out.println("-------------");
 
+        //TestHashLife
+        TestHashLife hashLife = new TestHashLife();
+        ok = ok && hashLife.testGetZero();
+        //ok = ok && hashLife.testCentre(); // en attente creation fonction equals
+        ok = ok && hashLife.testLife();
+        ok = ok && hashLife.testLife4x4();
+        ok = ok && hashLife.testSuccessor(); 
+        ok = ok && hashLife.testIsPadded();
+        ok = ok && hashLife.testPad();
+        ok = ok && hashLife.testInner();
+        ok = ok && hashLife.testCrop();
+        //ok = ok && hashLife.testAdvance();
 
-//         //TestHashLife
-//         TestHashLife hashLife = new TestHashLife();
-//         ok = ok && hashLife.testGetZero();
-//         ok = ok && hashLife.testCentre(); 
-//         ok = ok && hashLife.testLife();
-//         ok = ok && hashLife.testLife4x4();
-//         ok = ok && hashLife.testSuccessor(); 
-//         ok = ok && hashLife.testIsPadded();
-//         ok = ok && hashLife.testPad();
-//         ok = ok && hashLife.testInner();
-//         ok = ok && hashLife.testCrop();
-//         //ok = ok && hashLife.testAdvance();
     
-//         if (ok == false){
-//             System.out.println("At least one test KO");
-//         }
-//         else{
-//             System.out.println("All test OK");
-//         }
-//     }
-// }
+        if (ok == false){
+            System.out.println("At least one test KO");
+        }
+        else{
+            System.out.println("All test OK");
+        }
+    }
+}
+
